20bb391820fbc0e04c6c4196a9da61de97730569
DK\ili <ili@sitecore.net>
2017-02-09 11:39:53 +0200

    Release

---
+++ src/Sitecore.Support.146914/ResolveClientLanguage.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.LoggingIn;
+using Sitecore.Security.Accounts;
+using Sitecore.SecurityModel;
+using Sitecore.Sites;
+using System;
+using System.Linq;
+
+namespace Sitecore.Support.Pipelines
+{
+    public class ResolveClientLanguage
+    {
+
+        public virtual void Process(Sitecore.Pipelines.LoggingIn.LoggingInArgs args)
+        {
+            Sitecore.Diagnostics.Assert.ArgumentNotNull(args, "args");
+            if (!string.IsNullOrWhiteSpace(args.StartUrl))
+            {
+                return;
+            }
+            Sitecore.Security.Accounts.User user = Sitecore.Security.Accounts.User.FromName(args.Username, false);
+            string startUrl = user.Profile.StartUrl;
+            if (!string.IsNullOrWhiteSpace(startUrl))
+            {
+                args.StartUrl = startUrl;
+                return;
+            }
+            string startUrl2;
+            using (new Sitecore.SecurityModel.SecurityDisabler())
+            {
+                startUrl2 = Sitecore.Sites.SiteContextFactory.GetSiteInfo("shell").VirtualFolder;
+            }
+            args.StartUrl = startUrl2;
+        }
+    }
+}
